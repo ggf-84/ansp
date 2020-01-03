9c9209e7c36c154b8c1e59de0afc18544dcb55f3
ghostflp <gutughena@yahoo.com>
2020-01-03 15:17:20 +0000

    created xls parser

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -3,8 +3,8 @@
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
-
-        <title>Ansp</title>
+        
+        <title>{{ Voyager::setting("site.title") }}</title>
 
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
@@ -14,7 +14,7 @@
 
     </head>
     <body>
-        <div id="app"></div>
+        <div id="app" data=""></div>
         <script src="js/app.js"></script>
     </body>
     
